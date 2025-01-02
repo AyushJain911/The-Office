8c02103349bcdef89bbe14e988cdb1f4436d7b62
JainAyush911 <122337690+JainAyush911@users.noreply.github.com>
2025-01-02 13:03:13 +0530

    listed all the characters name and image

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div v-if="data">
+    <!-- <pre>{{ JSON.stringify(data, null, 2) }}</pre> -->
+    <ul v-for="(item, index) in data">
+      <li>{{ item.full_name }}</li>
+      <img v-bind:src="item.image" alt="" />
+    </ul>
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+import axios from "axios";
+
+const data = ref(null);
+
+// const OfficeApi = "https://platform.buildwithhussain.dev/api/method/get-office";
+
+const CORS_PROXY = "https://api.allorigins.win/raw?url="; //This is working fine
+const OfficeApi = `${CORS_PROXY}https://platform.buildwithhussain.dev/api/method/get-office`;
+
+axios
+  .get(OfficeApi)
+  .then((d) => (data.value = d.data.data))
+  .catch((err) => console.error("Error Fetching data", err));
+
+// fetch(OfficeApi)
+//   .then((d) => d.json())
+//   .then((msg) => console.log(msg));
+</script>
